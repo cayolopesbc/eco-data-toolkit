452092a8ba03edeb32230053116f88272610b224
Cayo Lopes <cayochalegre@ufrgs.br>
2020-05-28 17:50:14 -0300

    Added resample, temporal filter and point filter.

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ Created on Fri Apr 24 12:15:14 2020
 @author: cayoh
 """
 import xarray as xr
+import rioxarray as rxy
 import os
 import numpy as np
 import pandas as pd
@@ -26,34 +27,44 @@ import shapely
 
 class SpatialDataset:
     
-    def __init__(self, datapath):
-        
+    def __init__(self, datapath):    
         #Data Read:
-        
         ds   = xr.open_dataset(datapath)
-        self.data = ds.to_dataframe().reset_index()
-        self.variables = self.data.columns
+        self._data = ds.to_dataframe().reset_index() 
+        if self._data.select_dtypes(include=[np.datetime64]).keys()[0]:
+            self._data = self._data.set_index(self._data.select_dtypes(include=[np.datetime64]).keys()[0])
+        self._data['geometry'] =  self._getgeom()
+        self._variables = self._data.columns
+        self._crs  = {'init': 'epsg:4326'}
         
-#        try:
-#            self.geometry  = [shapely.geometry.Point(x,y) for x, y in zip(self.data['longitude'], self.data['latitude'])]
-#        except KeyError:
-#            self.geometry  = [shapely.geometry.Point(x,y) for x, y in zip(self.data['lon'], self.data['lat'])]
-            
-
+    @property
+    def data(self):
+        return self._data
+    
+    @property
+    def variables(self):
+        return self._variables
+    
+    def _df2geodf(self, geom, crs = None):
+        if crs != self._crs:
+            self._crs = crs
+        return gpd.GeoDataFrame(self._data, crs = self._crs, geometry = geom)
+    
+    def _getgeom(self):        
+        try:
+            geom = [Point(x,y) for x, y in zip(self._data['longitude'], self._data['latitude'])]
+        except KeyError:
+            geom = [Point(x,y) for x, y in zip(self._data['lon'], self._data['lat'])]
+            self._data['longitude'] = self._data['lon']
+            self._data['latitude']  = self._data['lat'] 
+        return geom
+        
     def var_choice(self, variables = []):
-        
-        # Checks whether a datetime column exists. If it exists, 
-        # it will be considered as a timeIndex.
-        
-        timestamp = [self.data.select_dtypes(include=[np.datetime64]).keys()[0]]
-        if len(timestamp)>0:
-            self.data = self.data.set_index(self.data.select_dtypes(include=[np.datetime64]).keys()[0])
-        
+
         #Variables list and choice:
-        
         if not variables:            
             selected = []
-            variables = np.array(self.data.columns)
+            variables = np.array(self._data.columns)
             i = 1
             print("The file have following variables:\n")
 
@@ -78,21 +89,21 @@ class SpatialDataset:
         else:
             selected = list(variables)
         
-        self.data = self.data[selected]
-        self.data = self.data.reset_index()
+        self._data = self._data[selected]
+
         try:
-            self.data = self.data.drop('index')
+            self._data = self._data.drop('index')
         except:
             pass
         
-        
+        self._variables = self._data.columns
         return
     
     def centroidFilter(self, mask_path, radius = 1):
-        
-        #Extraindo os dados  mais proximos da regiao de estudo para diminuir a quantidade
-        # de dados e agilizar o processamento:
-        
+        '''
+        It extracts the data closest to the study area to decrease 
+        the amount of data and speed up processing:
+        '''
         try:
             mask_shape = gpd.GeoDataFrame.from_file(mask_path)
         except:
@@ -105,22 +116,22 @@ class SpatialDataset:
         lon_filter_down = mask_shape.centroid.x + radius
         
         try:
-            self.data = self.data[(lon_filter_down[0] >= self.data['longitude']) & (self.data['longitude'] >= lon_filter_up[0]) & (lat_filter_down[0] >= self.data['latitude']) & (self.data['latitude'] >= lat_filter_up[0])]
+            self._data = self._data[(lon_filter_down[0] >= self._data['longitude']) & (self._data['longitude'] >= lon_filter_up[0]) & (lat_filter_down[0] >= self._data['latitude']) & (self._data['latitude'] >= lat_filter_up[0])]
         except KeyError:
-            self.data = self.data[(lon_filter_down[0] >= self.data['lon']) & (self.data['lon'] >= lon_filter_up[0]) & (lat_filter_down[0] >= self.data['lat']) & (self.data['lat'] >= lat_filter_up[0])]
-            self.data['longitude'] = self.data['lon']
-            self.data['latitude'] = self.data['lat']
+            self._data = self._data[(lon_filter_down[0] >= self._data['lon']) & (self._data['lon'] >= lon_filter_up[0]) & (lat_filter_down[0] >= self._data['lat']) & (self._data['lat'] >= lat_filter_up[0])]
+            self._data['longitude'] = self._data['lon']
+            self._data['latitude'] = self._data['lat']
             
-        geom = [shapely.geometry.Point(x,y) for x, y in zip(self.data['longitude'], self.data['latitude'])]
-        self.data = gpd.GeoDataFrame(self.data, geometry = geom)
-        self.data.crs = mask_shape.crs
+        geom =  self._getgeom()
+        crs = mask_shape.crs
+        self._data = self._df2geodf(geom, crs)
         return
         
     def pointFilter(self, Coord):
-        
-        #Find the nearest grid point to specified coordinates 
-        # and extract data.
-        
+        '''
+        Find the nearest grid point to specified coordinates 
+         and extract data.
+        '''
         if isinstance(Coord,list):
             Point = shapely.geometry.Point(Coord[0], Coord[1])
         elif isinstance(Coord, shapely.geometry.Point):
@@ -129,73 +140,95 @@ class SpatialDataset:
             print("Invalid coordinates format.")
             return
             
-        try:
-            geom = [shapely.geometry.Point(x,y) for x, y in zip(self.data['longitude'], self.data['latitude'])]
-        except KeyError:
-            geom = [shapely.geometry.Point(x,y) for x, y in zip(self.data['lon'], self.data['lat'])]   
-            
-        geom = list(set(geom))
-
-        geomei = shapely.ops.nearest_points(Point.centroid, shapely.geometry.MultiPoint(geom))
-        self.data = self.data.geometry.intersects(geomei[1])
-        return self.data
+        if isinstance(self._data,gpd.GeoDataFrame):
+            geom = list(self._data.geometry)           
+        else:
+            geom = self._getgeom()
+            self._data = self._df2geodf(geom)
+                
+        geomeInt = shapely.ops.nearest_points(Point.centroid, shapely.geometry.MultiPoint(geom))
+        self._data = self._data[self._data.geometry.intersects(geomeInt[1])]
+        return
      
     def temporalFilter(self, StartDate, EndDate):
-        
-        # Filter time series between specific dates.
-        
-        self.data = self.data[StartDate:EndDate]
+        '''
+        Makes a temporal filter in dataset.
+        '''
+        if isinstance(self._data.index,pd.DatetimeIndex):
+            self._data = self._data[StartDate:EndDate]
+        else:
+            print('No has DateTime index in the dataset.')
         return 
 
+    def resample(self, freq = '1H', method = 'ffill', order = 1):
 
-
-
-  
-def resampleMI(data, freq = '0.5H', method = 'ffill'):
-# .mean()
-# .ffill()
-# .bbfill()
-# .sum()    
-    try:
-        data_crs = data.crs
-    except AttributeError:
-        data_crs = {'init': 'epsg:4326'}
-    
-    #Converte GeoDataFrame em DataFrame para utilizar o metodo .resample():
-    if isinstance(data, pd.DataFrame):    
-        data = pd.DataFrame(data)
-    
-    #Filtra as geometrias para iniciar o resample:
-    geometry = data[['geometry']].drop_duplicates(subset = ['geometry'])
-    
-    #De acordo com a geometria (coordenadas) e feito o resample de sua respectiva
-    #serie temporal e posteriormente todas as series novas sao unidas em um novo
-    #dataframe
-    
-    resamples = []
-    for geom in geometry['geometry']:
-        filt = data[(data['geometry'] == geom)]
-        resample_data = filt.resample(freq).ffill()
-        resamples.append(resample_data)
-    df = pd.concat(resamples)
-
-    #Convertendo o novo DF em GeoDataFrame:    
-    df = df.drop(['geometry'], axis = 1)
-
-    try:
-        geom = [Point(x,y) for x, y in zip(df['longitude'], df['latitude'])]
-    except KeyError:
-        geom = [Point(x,y) for x, y in zip(df['lon'], df['lat'])]
+        #Converte GeoDataFrame em DataFrame para utilizar o metodo .resample():
+        if isinstance(self._data, gpd.GeoDataFrame):    
+            self._data = pd.DataFrame(self._data)
+          
+        #Filtra as geometrias para iniciar o resample:
+        geometry = self._data['geometry'].drop_duplicates()
         
-    df = gpd.GeoDataFrame(df, crs = data_crs, geometry = geom)
-        
-    return df     
+        #De acordo com a geometria (coordenadas) e feito o resample de sua respectiva
+        #serie temporal e posteriormente todas as series novas sao unidas em um novo
+        #dataframe
+        resamples = []
+        for geoPoint in geometry:
+            
+            resample_data = self._data[(self._data['geometry'] == geoPoint)].resample(freq).mean()
+            
+            if method == 'polynomial' or method == 'spline':
+                resample_data = resample_data.interpolate(method = method, order = order)   
+            elif method=='backfill' or method=='bfill':
+                resample_data = resample_data.bbfill()            
+            else:
+                resample_data = resample_data.ffill()
+            resamples.append(resample_data)  
 
-def pointFilter(df, Coord):
-    geom = df.geometry.drop_duplicates()
-    Point = Point(Coord[0], Coord[1])
-    geomei = shapely.ops.nearest_points(Point.centroid, MultiPoint(geom.values))
-    return df[df.geometry.intersects(geomei[1])]
+        self._data = pd.concat(resamples)       
+        
+        geom = self._getgeom()
+        self._data = self._df2geodf(geom)
+        return
+
+
+
+# Preenche as falhas das séries, sejam essas falhas oriundas dos dados de origem ou devido ao aumento da frequência utilizando a função groupData
+# Métodos de preenchimento dos dados faltantes:
+# Apenas substituicao:
+# pad / ffill: propagate last valid observation forward to next valid 
+# backfill / bfill: use next valid observation to fill gap.
+        
+# 'ffill'       - Propaga um valor conhecido "para frente" até um encontrar um novo valor conhecido.
+# 'backfill'    - Propaga um valor conhecido "para trás" até um encontrar um novo valor conhecido.
+# Interpolacoes:
+# 'polynomial'  - Interpolação polinomial, deve se informar uma ordem (order = n): a) ordem máxima para os dados: 4; b) Ordem = 1 retorna uma interpolacao linear.
+# 'spline'      - Interpolação usando splines, deve se informar uma ordem (order = n): a) ordem máxima para esses dados: 5; sugiro ordem 1 ou 3, melhor ler mais sobre o método.
+
+
+
+def cropRst(raster, mask_shp, out_tif = None, remove = False, buffer_mask = 0):
+    if isinstance(mask_shp,gpd.GeoDataFrame):
+      pass  
+    else:
+        mask_shp = gpd.GeoDataFrame.from_file(mask_shp)
+        mask_shp.geometry[0] = mask_shp.geometry[0].buffer(buffer_mask)
+   
+    if out_tif == None:
+        out_tif = raster.replace('.tif','_crop.tif')
+    
+    
+    ds = rxy.open_rasterio(raster, masked = True, chunks = True)
+    clipped = ds.rio.clip(mask_shp.geometry.apply(mapping), mask_shp.crs, drop=False, invert=False)
+    clipped.rio.to_raster(out_tif)
+    ds.close() 
+    
+    if remove:
+        os.remove(raster) 
+        print("Aqui02")
+          
+    return out_tif
+
 
 def pts2Raster(df, var_list, base_name, out_dir, outputBounds = [-52.40,-33.80,-53.80,-32.00], outCRS = 'WGS84', buffer_mask = 0, shp_mask = None):
     outs = []
@@ -258,7 +291,7 @@ ei = gpd.GeoDataFrame(ei, crs = crs, geometry = geomei)
 #Temporal Filter:
 #Insert dates on format Month/Day/Year
 StartDate = "01/21/2000"
-EndDate   =  "02/21/2000"
+EndDate   =  "01/21/2000"
 
 ei = temporalFilter(df, StartDate, EndDate)
 
