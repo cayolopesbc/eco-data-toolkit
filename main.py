f4555933520c82792a8cf331c64917d1c676a979
Cayo Lopes <cayochalegre@ufrgs.br>
2020-05-28 22:18:24 -0300

    Inserted pts2csv and ptsTime2raster methods.

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ import pandas as pd
 import geopandas as gpd
 import datetime
 
-from shapely.geometry import Point, MultiPoint
+from shapely.geometry import Point, MultiPoint, mapping
 
 import shapely
 ################################################################################
@@ -27,17 +27,21 @@ import shapely
 
 class SpatialDataset:
     
-    def __init__(self, datapath):    
+    def __init__(self, datapath, crs ={'init': 'epsg:4326'} ):    
         '''
-        class constructor.
+        class constructor
         '''
         ds   = xr.open_dataset(datapath)
         self._data = ds.to_dataframe().reset_index() 
         if self._data.select_dtypes(include=[np.datetime64]).keys()[0]:
             self._data = self._data.set_index(self._data.select_dtypes(include=[np.datetime64]).keys()[0])
+        
         self._data['geometry'] =  self._getgeom()
+        self._crs  = crs
+        
+        self._data = self._df2geodf(self._data['geometry'], crs = self._crs)
         self._variables = self._data.columns
-        self._crs  = {'init': 'epsg:4326'}
+        
         
     @property
     def data(self):
@@ -46,6 +50,7 @@ class SpatialDataset:
     @property
     def variables(self):
         return self._variables
+    
     
     def _df2geodf(self, geom, crs = None):
         if crs != self._crs:
@@ -56,9 +61,15 @@ class SpatialDataset:
         try:
             geom = [Point(x,y) for x, y in zip(self._data['longitude'], self._data['latitude'])]
         except KeyError:
-            geom = [Point(x,y) for x, y in zip(self._data['lon'], self._data['lat'])]
-            self._data['longitude'] = self._data['lon']
-            self._data['latitude']  = self._data['lat'] 
+            try:
+                geom = [Point(x,y) for x, y in zip(self._data['lon'], self._data['lat'])]
+                self._data['longitude'] = self._data['lon']
+                self._data['latitude']  = self._data['lat']
+            except KeyError:
+                geom = [Point(x,y) for x, y in zip(self._data['x'], self._data['y'])]
+                self._data['longitude'] = self._data['lon']
+                self._data['latitude']  = self._data['lat']
+             
         return geom
         
     def var_choice(self, variables = []):
@@ -188,7 +199,7 @@ class SpatialDataset:
      
     def temporalFilter(self, StartDate, EndDate):
         '''
-       This method makes a temporal filter in dataset.
+       This method applies a temporal filter in dataset.
         
         input:
             :param StarDate =  initial timestamp 
@@ -205,7 +216,7 @@ class SpatialDataset:
 
     def resample(self, freq = '1H', method = 'ffill', order = 1):
         '''
-        This method makes a temporal resample.
+        This method applies a temporal resample.
         
         input:
             :param freq = resample frequencie, default frequencie is set as freq = '1H'
@@ -230,16 +241,11 @@ class SpatialDataset:
         
         '''
 
-        #Converte GeoDataFrame em DataFrame para utilizar o metodo .resample():
         if isinstance(self._data, gpd.GeoDataFrame):    
             self._data = pd.DataFrame(self._data)
-          
-        #Filtra as geometrias para iniciar o resample:
+
         geometry = self._data['geometry'].drop_duplicates()
-        
-        #De acordo com a geometria (coordenadas) e feito o resample de sua respectiva
-        #serie temporal e posteriormente todas as series novas sao unidas em um novo
-        #dataframe
+ 
         resamples = []
         for geoPoint in geometry:
             
@@ -258,54 +264,173 @@ class SpatialDataset:
         geom = self._getgeom()
         self._data = self._df2geodf(geom)
         return
-
-
-
-# Preenche as falhas das séries, sejam essas falhas oriundas dos dados de origem ou devido ao aumento da frequência utilizando a função groupData
-# Métodos de preenchimento dos dados faltantes:
-# Apenas substituicao:
-# pad / ffill: propagate last valid observation forward to next valid 
-# backfill / bfill: use next valid observation to fill gap.
+    
+    def _maskCheck(self, shape, buffer_mask):
+        if not isinstance(mask_shp,gpd.GeoDataFrame):
+            mask_shp = gpd.GeoDataFrame.from_file(mask_shp)
+            mask_shp.geometry[0] = mask_shp.geometry[0].buffer(buffer_mask)
+        return mask_shp
         
-# 'ffill'       - Propaga um valor conhecido "para frente" até um encontrar um novo valor conhecido.
-# 'backfill'    - Propaga um valor conhecido "para trás" até um encontrar um novo valor conhecido.
-# Interpolacoes:
-# 'polynomial'  - Interpolação polinomial, deve se informar uma ordem (order = n): a) ordem máxima para os dados: 4; b) Ordem = 1 retorna uma interpolacao linear.
-# 'spline'      - Interpolação usando splines, deve se informar uma ordem (order = n): a) ordem máxima para esses dados: 5; sugiro ordem 1 ou 3, melhor ler mais sobre o método.
+    def pts2csv(self, out_name):
+        '''
+        This method imports the points dataset to comma separete file (*.csv).
+        
+        input:
+            param: out_name = output file name.
+        output:
+            return: a .csv file.
+        
+        info:
+            This method can apply in cases where its wish to work with data at one point.
+            
+        '''
+        
+        aux = self._data
+        aux.drop('geometry', axis = 1, inplace = True)
+        aux.to_csv(out_name)
+        return
+
+    def _cropRst(self,raster, mask_shp, out_tif = None, remove = False, buffer_mask = 0):
+        
+        mask_shp = self._maskCheck(mask_shp, buffer_mask)
+       
+        if out_tif == None:
+            out_tif = raster.replace('.tif','_crop.tif')
+        
+        with rxy.open_rasterio(raster, masked = True, chunks = True):
+            clipped = ds.rio.clip(mask_shp.geometry.apply(mapping), mask_shp.crs, drop=False, invert=False)
+            clipped.rio.to_raster(out_tif)
+        
+        if remove:
+            os.remove(raster)             
+        return out_tif
+
+
+    def ptsTime2Raster(self, out_name, var_list = None, outputBounds = None, outCRS = 'WGS84', mask_shp = None, buffer_mask = 0):
+        
+        '''
+        This method convert points time series in rasters data series by linear interpolation.
+        
+        input:
+            :param out_name  = output name base name
+                If out_name = 'D:/output/dir/path/raster_name.tif',this implies that the output 
+                raster file names will have the following format: 
+                'D:/output/dir/path/raster_name_time_stamp_variableName.tif')
+                
+            :param var_list (optional) = variables list to convert in rasters files.
+                If not informed, all available variables in dataset will be converted.
+                
+           
+            :param outputBounds (optional) = is set as a list instance with the following format: 
+                
+                [upperLeft Longitude, upperLeft Latitude, lowerRight Longitude, lowerRight Latitude]
+                
+                This defines the interpolation area in a rectangle delimited by its upper left point 
+                coordinates and lower right point coordinates.
+                
+                default value: the rectangle area is defined by upper left and lower right dataset 
+                coordinates increased by 1%.
+                
+                
+            :param outCRS (optional) = output Coordinate Reference System.
+                                        default value: WGS84.
+            
+            :param mask_shp (optional) = If informed, the interpolation raster is croped to shapefile boundaries.
+            
+            :param buffer_mask (optional) = buffer in shapefile mask area ( in %percentage).
+                                            It is only used if the mask shapefile is inserted.
+                                            default value: 0%.
+        
+        output:
+            return: multiples raster files ( format .tif)
+        
+        '''
+        
+        if not var_list:
+            var_list = self._data.columns.drop(['latitude', 'longitude', 'geometry'], errors = 'ignore')
+                
+        if not outputBounds:
+            geom = self._data.geometry.drop_duplicates()
+            outputBounds = [x*1.02 for x in [geom.x.max(), geom.y.min(), geom.x.min(), geom.y.max()]]
+            
+        if '.tif' in out_name:
+            out_name = out_name.replace('.tif','')
+            
+        out_dir = '\\'.join(out_name.split('\\')[:-1])
+        timeSteps = self._data.sort_index().index.drop_duplicates()
+        
+        for time in timeSteps:
+            timeName = str(t).replace(' ','_').replace('-','_').replace(':','_')
+        
+            for varName in var_list:
+                vrt_fn = os.path.join(out_dir,varName+'Vrt.vrt')
+                lyr_name = varName
+                out_tif = '_'.join([out_name,varName,timeName,'.tif'])
+                tempPath = os.path.join(out_dir, varName +'.csv')
+                self._data[[varName,'latitude','longitude']].loc[time].to_csv(tempPath,header = True, index = False)
+                
+                with open(vrt_fn, 'w') as fn_vrt:
+                    fn_vrt.write('<OGRVRTDataSource>\n')
+                    fn_vrt.write('\t<OGRVRTLayer name="%s">\n' % lyr_name)
+                    fn_vrt.write('\t\t<SrcDataSource>%s</SrcDataSource>\n' % tempPath)
+                    fn_vrt.write('\t\t<SrcLayer>%s</SrcLayer>\n' % lyr_name)
+                    fn_vrt.write('\t\t<GeometryType>wkbPoint</GeometryType>\n')
+                    fn_vrt.write('\t\t<GeometryField encoding="PointFromColumns" x="longitude" y="latitude" z="%s"/>\n'  %varName)
+                    fn_vrt.write('\t</OGRVRTLayer>\n')
+                    fn_vrt.write('</OGRVRTDataSource>\n')
+                
+                gridOp = gdal.GridOptions(format = 'Gtiff', outputBounds = outputBounds, algorithm = 'linear:radius=0.0:nodata = -9999', outputSRS = outCRS)
+                
+                if isinstance(shp_mask,gpd.GeoDataFrame):
+                    temp_tif = out_name + '_' + varName +'.tif'
+                    gdal.Grid(temp_tif, vrt_fn, options = gridOp)
+                    self._cropRst(temp_tif, mask_shp, out_tif, remove = True, buffer_mask = buffer_mask)
+                else:
+                    gdal.Grid(out_tif, vrt_fn, options = gridOp)
+                
+                os.remove(tempPath)
+                os.remove(vrt_fn)
+        return 
 
 
 
-def cropRst(raster, mask_shp, out_tif = None, remove = False, buffer_mask = 0):
-    if isinstance(mask_shp,gpd.GeoDataFrame):
-      pass  
-    else:
-        mask_shp = gpd.GeoDataFrame.from_file(mask_shp)
-        mask_shp.geometry[0] = mask_shp.geometry[0].buffer(buffer_mask)
-   
-    if out_tif == None:
-        out_tif = raster.replace('.tif','_crop.tif')
+
+
+
+
+
+
+[-52.40,-33.80,-54.00,-32.00]
+
+[geom.x.max(), geom.y.min(), geom.x.min(), geom.y.max()]
+
+
+geom = self._data.geometry.drop_duplicates()
+
+shapes = "D:\\OneDrive\\Arquivos OLD PC\\Cafofo\\Balanco de Calor\\MangueiraShp\\polimirim.shp"
+
+data = rasterio.open(rst)
+shapes = gpd.GeoDataFrame.from_file(shapes)
+
+shapes = shapes.to_crs(crs=data.crs.data)
+
+
+def pts2Raster(self, out_name, var_list = = None, outputBounds = None, outCRS = 'WGS84', buffer_mask = 0, shp_mask = None):
     
-    
-    ds = rxy.open_rasterio(raster, masked = True, chunks = True)
-    clipped = ds.rio.clip(mask_shp.geometry.apply(mapping), mask_shp.crs, drop=False, invert=False)
-    clipped.rio.to_raster(out_tif)
-    ds.close() 
-    
-    if remove:
-        os.remove(raster) 
-        print("Aqui02")
-          
-    return out_tif
-
-
-def pts2Raster(df, var_list, base_name, out_dir, outputBounds = [-52.40,-33.80,-53.80,-32.00], outCRS = 'WGS84', buffer_mask = 0, shp_mask = None):
-    outs = []
+    if not var_list:
+        var_list = self._data.columns.drop(['latitude', 'longitude', 'geometry'], errors = 'ignore')
+            
+    if not outputBounds:
+        geom = self._data.geometry.drop_duplicates()
+        outputBounds = [x*1.02 for x in [geom.x.max(), geom.y.min(), geom.x.min(), geom.y.max()]]
+      
+    out_dir = '\\'.join(os.getcwd().split('\\')[:-1])
     for varName in var_list:
         vrt_fn = os.path.join(out_dir,varName+'Vrt.vrt')
         lyr_name = varName
-        out_tif = os.path.join(out_dir, base_name + '_'+ varName +'.tif')
+        out_tif = os.path.join(out_name + '_'+ varName +'.tif')
         tempPath = os.path.join(out_dir, varName +'.csv')
-        df[[varName,'latitude','longitude']].to_csv(tempPath,header = True, index = False)
+        self._data[[varName,'latitude','longitude']].to_csv(tempPath,header = True, index = False)
         
         with open(vrt_fn, 'w') as fn_vrt:
             fn_vrt.write('<OGRVRTDataSource>\n')
@@ -317,20 +442,18 @@ def pts2Raster(df, var_list, base_name, out_dir, outputBounds = [-52.40,-33.80,-
             fn_vrt.write('\t</OGRVRTLayer>\n')
             fn_vrt.write('</OGRVRTDataSource>\n')
         
-#        https://gdal.org/programs/gdal_grid.html
         gridOp = gdal.GridOptions(format = 'Gtiff', outputBounds = outputBounds, algorithm = 'linear:radius=0.0:nodata = -9999', outputSRS = outCRS)
         
         if isinstance(shp_mask,gpd.GeoDataFrame):
-            temp_tif = os.path.join(out_dir,"Temp_" + base_name + '_'+ varName +'.tif')
+            temp_tif = out_name + '_' + varName +'.tif'
             gdal.Grid(temp_tif, vrt_fn, options = gridOp)
             cropRst(temp_tif, shp_mask, out_tif, remove = True, buffer_mask = buffer_mask)
         else:
             gdal.Grid(out_tif, vrt_fn, options = gridOp)
         
         os.remove(tempPath)
-        outs.append(out_tif)
-
-    return outs
+        os.remove(vrt_fn)
+    return 
 
 
 ################################
